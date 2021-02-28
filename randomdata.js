19e6b04258db2ed648c53cb0f01883389652f95a
oddaaron00 <caseygoodings@outlook.com>
2021-02-28 12:19:47 +0000

    Update randomdata.js

--- randomdata.js
+++ randomdata.js
@@ -40,5 +40,5 @@ exports.pickTime = function(type = 'hour') {
 }
 
 exports.pickHash = function() {
-    return [...Array(32)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+    return [...Array(32)].map(() => Math.floor(Math.random() * 16).toString(16)).join("");
 }
